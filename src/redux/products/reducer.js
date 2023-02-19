d7d6fa3704c729436b518fc9f8a46d485dab6bf0
FriyadDev <riyadhasanblue2323@gmail.com>
2023-02-20 00:34:22 +0600

    all products data implemented to the UI

--- src/redux/products/reducer.js
+++ src/redux/products/reducer.js
@@ -2,30 +2,30 @@ import { ADDPRODUCT } from "./actionTypes";
 
 const initialState = {
   allProducts: [
-    {
-      id: 1,
-      imgURL: "https://i.dummyjson.com/data/products/59/thumbnail.jpg",
-      title: "Spring and summershoes",
-      category: "Mens shoes",
-      price: 400,
-      quantity: 10,
-    },
-    {
-      id: 2,
-      imgURL: "https://i.dummyjson.com/data/products/59/thumbnail.jpg",
-      title: "Spring and summershoes",
-      category: "Mens shoes",
-      price: 400,
-      quantity: 10,
-    },
-    {
-      id: 3,
-      imgURL: "https://i.dummyjson.com/data/products/59/thumbnail.jpg",
-      title: "Spring and summershoes",
-      category: "Mens shoes",
-      price: 400,
-      quantity: 10,
-    },
+    // {
+    //   id: 1,
+    //   imgURL: "https://i.dummyjson.com/data/products/59/thumbnail.jpg",
+    //   title: "Spring and summershoes",
+    //   category: "Mens shoes",
+    //   price: 400,
+    //   quantity: 10,
+    // },
+    // {
+    //   id: 2,
+    //   imgURL: "https://i.dummyjson.com/data/products/59/thumbnail.jpg",
+    //   title: "Spring and summershoes",
+    //   category: "Mens shoes",
+    //   price: 400,
+    //   quantity: 10,
+    // },
+    // {
+    //   id: 3,
+    //   imgURL: "https://i.dummyjson.com/data/products/59/thumbnail.jpg",
+    //   title: "Spring and summershoes",
+    //   category: "Mens shoes",
+    //   price: 400,
+    //   quantity: 10,
+    // },
   ],
 };
 
--- src/sections/ProductsContainer.jsx
+++ src/sections/ProductsContainer.jsx
@@ -1,50 +1,37 @@
+import { useSelector } from "react-redux";
+
 const ProductsContainer = () => {
+  const { allProducts } = useSelector((state) => state.products);
+
   return (
     <div className="productContainer" id="lws-productContainer">
-      {/* product item */}
-      <div className="lws-productCard">
-        <img
-          className="lws-productImage"
-          src="https://i.dummyjson.com/data/products/59/thumbnail.jpg"
-          alt="product"
-        />
-        <div className="p-4 space-y-2">
-          <h4 className="lws-productName">Spring and summershoes</h4>
-          <p className="lws-productCategory">Mens shoes</p>
-          <div className="flex items-center justify-between pb-2">
-            <p className="productPrice">
-              BDT <span className="lws-price">400</span>
-            </p>
-            <p className="productQuantity">
-              QTY <span className="lws-quantity">10</span>
-            </p>
-          </div>
-          <button className="lws-btnAddToCart">Add To Cart</button>
+      {allProducts.length > 0 ? (
+        allProducts.map((item) => {
+          const { id, imgURL, title, category, price, quantity } = item;
+          return (
+            <div key={id} className="lws-productCard">
+              <img className="lws-productImage" src={imgURL} alt="product" />
+              <div className="p-4 space-y-2">
+                <h4 className="lws-productName">{title}</h4>
+                <p className="lws-productCategory">{category}</p>
+                <div className="flex items-center justify-between pb-2">
+                  <p className="productPrice">
+                    BDT <span className="lws-price">{price}</span>
+                  </p>
+                  <p className="productQuantity">
+                    QTY <span className="lws-quantity">{quantity}</span>
+                  </p>
+                </div>
+                <button className="lws-btnAddToCart">Add To Cart</button>
+              </div>
+            </div>
+          );
+        })
+      ) : (
+        <div>
+          <p>No product found</p>
         </div>
-      </div>
-      {/* product item ends */}
-      {/* product item */}
-      <div className="lws-productCard">
-        <img
-          className="lws-productImage"
-          src="https://i.dummyjson.com/data/products/40/thumbnail.jpg"
-          alt="product"
-        />
-        <div className="p-4 space-y-2">
-          <h4 className="lws-productName">Women Winter Clothes</h4>
-          <p className="lws-productCategory">Tops</p>
-          <div className="flex items-center justify-between pb-2">
-            <p className="productPrice">
-              BDT <span className="lws-price">100</span>
-            </p>
-            <p className="productQuantity">
-              QTY <span className="lws-quantity">30</span>
-            </p>
-          </div>
-          <button className="lws-btnAddToCart">Add To Cart</button>
-        </div>
-      </div>
-      {/* product item ends */}
+      )}
     </div>
   );
 };
